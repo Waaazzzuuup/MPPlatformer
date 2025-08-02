5a51336c1aea00998df0fce315216844d9b1df96
Waaazzzuuup <Waaazzzuuup@users.noreply.github.com>
2025-08-02 23:59:54 +0300

    update SESSION_NAME
    so basically these capital fine gents still do not update NUM_PUBLIC_CONNECTIONS in Session if the name of the session is any different from default
    for 4.smth++ it is "GameSession". Earlier it was "Game". gg.

--- Source/MPPlatformer/PuzzlePlatformsGameInstance.cpp
+++ Source/MPPlatformer/PuzzlePlatformsGameInstance.cpp
@@ -13,7 +13,7 @@
 #include "MenuSystem/MainMenu.h"
 #include "MenuSystem/GameMenu.h"
 
-const static FName SESSION_NAME = TEXT("MySessionGame");
+const static FName SESSION_NAME = TEXT("GameSession");
 const static FName SERVER_NAME_SETTINGS_KEY = TEXT("CustomServerName");
 
 
